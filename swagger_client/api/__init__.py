e7d172b17b8ba984358391bc6a34d480e9432f44
Petr Beranek <petrberanek.mail@gmail.com>
2022-11-06 14:41:33 +0100

    Initial commit
    Client SDK automatically generated from
    https://app.swaggerhub.com/apis/Creditas/OpenAPI/1.0.0

---
+++ swagger_client/api/__init__.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from swagger_client.api.account_api import AccountApi
+from swagger_client.api.administration_api import AdministrationApi
+from swagger_client.api.aisp_api import AispApi
+from swagger_client.api.authorization_api import AuthorizationApi
+from swagger_client.api.balance_api import BalanceApi
+from swagger_client.api.card_api import CardApi
+from swagger_client.api.cisp_api import CispApi
+from swagger_client.api.example_api import ExampleApi
+from swagger_client.api.loan_api import LoanApi
+from swagger_client.api.payment_api import PaymentApi
+from swagger_client.api.pisp_api import PispApi
+from swagger_client.api.statement_api import StatementApi
+from swagger_client.api.transaction_api import TransactionApi
